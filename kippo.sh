db78f301acfe8847699b0b1ab93b00f9089891f5
petersmatt <peters.matthew.fx@gmail.com>
2015-01-05 08:49:52 -0500

    Setup bare minimum install

--- kippo.sh
+++ kippo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+$USERNAME=$1
+$PORT=$2
 apt-get install -y python-dev openssl python-openssl python-pyasn1 python-twisted subversion
 
+mkdir /opt/kippo
+chown $USERNAME /opt/hp/kippo
+
+svn checkout http://kippo.googlecode.com/svn/trunk/ /opt/kippo
