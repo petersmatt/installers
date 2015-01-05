bcf2315c7bd168c97f553ef9ef3bc1b769f8174b
petersmatt <peters.matthew.fx@gmail.com>
2015-01-05 09:27:22 -0500

    Added kippo user creation and authbind params

--- kippo.sh
+++ kippo.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-$USERNAME=$1
-$PORT=$2
+PASS=$1
+PORT=$2
+
 apt-get install -y python-dev openssl python-openssl python-pyasn1 python-twisted subversion
 
-mkdir /opt/kippo
-chown $USERNAME /opt/hp/kippo
+#setup authbind
+useradd kippo
+CHANGEPASS="kippo:$PASS"
+echo $CHANGEPASS | chpasswd
 
+touch /etc/authbind/byport/$PORT
+chown $USERNAME /etc/authbind/byport/$PORT
+chmod 777 /etc/authbind/byport/$PORT
+
+#make directory and set perms
+mkdir /opt/kippo
+chown kippo /opt/hp/kippo
+
+
+#download kippo files
 svn checkout http://kippo.googlecode.com/svn/trunk/ /opt/kippo
